7f49ea989969e32c10d04d2bfec46ffd20acbd91
cherin-yoo <qhfk3491@naver.com>
2025-09-07 14:55:31 +0900

    [1543]문서 검색 - 복습필요

---
+++ src/ch01_string/SearchDoc.java
@@ -0,0 +1,25 @@
+package ch01_string;
+
+import java.util.Scanner;
+
+public class SearchDoc {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String doc = sc.nextLine();
+		String word  = sc.nextLine();
+		
+		int count = 0;
+		int startIndex = 0;
+		while(  true ) {
+			int findIndex = doc.indexOf(word, startIndex);
+			if( findIndex < 0) break;
+			
+			count++;
+			startIndex = findIndex + word.length();
+		}
+		
+		System.out.println(count);
+	}
+
+}
