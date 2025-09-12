305f9a66a3fafc453516b7c825642970bb7afd80
cherin-yoo <qhfk3491@naver.com>
2025-09-12 23:14:17 +0900

    [10158]개미

---
+++ src/ch02_time/Ant.java
@@ -0,0 +1,26 @@
+package ch02_time;
+
+import java.util.Scanner;
+
+public class Ant {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int W = sc.nextInt();
+		int H = sc.nextInt();
+		int P = sc.nextInt();
+		int Q = sc.nextInt();
+		int T = sc.nextInt();
+		
+		int currentX = (T+P)%(2*W);
+		int currentY = (T+Q)%(2*H);
+		
+		if(currentX > W) currentX = 2*W-currentX;
+		if(currentY > H) currentY = 2*H-currentY;
+		
+		System.out.println( currentX + " " + currentY );
+	}
+
+}
+
