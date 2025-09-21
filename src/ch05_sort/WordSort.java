d4b1aac2144c01d067517253aad2d46b86f15486
cherin-yoo <qhfk3491@naver.com>
2025-09-21 11:11:43 +0900

    [1181]단어정렬

---
+++ src/ch05_sort/WordSort.java
@@ -0,0 +1,39 @@
+package ch05_sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class WordSort {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();	// 단어의 개수
+		
+		String[] words = new String[N];
+		for(int i=0; i<N; i++) {
+			words[i] = sc.next();
+		}
+		
+		Arrays.sort(words, new Comparator<String>(){
+			@Override
+			public int compare(String o1, String o2) {
+					// 길이가 같으면 사전 순으로 정렬
+					if(o1.length()==o2.length()) {
+						return o1.compareTo(o2);
+					}
+				// 길이가 짧은 것 부터 정렬
+				return o1.length() - o2.length();
+			}
+		});
+		
+		System.out.println(words[0]);
+		for(int i=0; i<words.length; i++) {
+			// 중복 제거
+			if(i!=0 && !words[i].equals(words[i-1])) {
+				System.out.println(words[i]);
+			}
+		}
+	}
+
+}
